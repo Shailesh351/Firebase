25fcd19a38efca5b2bd7918525945ad0d4e771e8
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2016-10-10 10:27:03 +0530

    Add Authentication using Goodgle Sign In(Missing file)

--- Firebase Auth/SimplifiedSignIn/app/src/main/java/com/shell/simplifiedsignin/LoginActivity.java
+++ Firebase Auth/SimplifiedSignIn/app/src/main/java/com/shell/simplifiedsignin/LoginActivity.java
@@ -11,23 +11,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final int RC_SIGN_IN = 9001;
     private TextInputLayout mTextInputLayoutEmail, mTextInputLayoutPassword;
     private EditText mEditTextEmail, mEditTextPassword;
     private Button mButtonSignUp, mButtonLogin, mButtonResetPassword;
     private ProgressBar mProgressbar;
     private FirebaseAuth mAuth;
+
+    //Google variables
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +65,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mButtonLogin = (Button) findViewById(R.id.btn_login);
         mButtonResetPassword = (Button) findViewById(R.id.btn_reset_password);
 
+        signInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+        setGoogleSignInButtonText(signInButton, "Sign in with Google");
+
         mButtonSignUp.setOnClickListener(this);
         mButtonLogin.setOnClickListener(this);
         mButtonResetPassword.setOnClickListener(this);
+        signInButton.setOnClickListener(this);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         mProgressbar = (ProgressBar) findViewById(R.id.progressBar);
     }
@@ -69,11 +99,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.btn_reset_password:
                 startActivity(new Intent(this, ResetPasswordActivity.class));
                 return;
+            case R.id.google_sign_in_button:
+                signInWithGoogle();
+                return;
             default:
                 Toast.makeText(LoginActivity.this, "No Click", Toast.LENGTH_SHORT).show();
         }
     }
 
+    //Sign In with Email and Password
     private void signIn() {
         String email = mEditTextEmail.getText().toString().trim();
         String password = mEditTextPassword.getText().toString().trim();
@@ -97,6 +131,52 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         });
 
         mProgressbar.setVisibility(View.GONE);
+    }
+
+    //Sign In with Google
+    private void signInWithGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "Google Sign In failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Signed In.",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
     }
 
     //Validate Email and Password Field
@@ -151,5 +231,30 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onResume() {
         super.onResume();
         mProgressbar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
+    }
+
+    //Change text of google sign in button
+    private void setGoogleSignInButtonText(SignInButton signInButton, String buttonText) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText(buttonText);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 }
